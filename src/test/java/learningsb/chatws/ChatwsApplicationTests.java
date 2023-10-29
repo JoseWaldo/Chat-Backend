caac6dee0a2ebdccc634d8e446835a7913c19f5f
Jose Waldo <josferw@gmail.com>
2023-10-29 17:32:45 -0500

    Inicio del proyecto sobre websocket

---
+++ src/test/java/learningsb/chatws/ChatwsApplicationTests.java
@@ -0,0 +1,13 @@
+package learningsb.chatws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
